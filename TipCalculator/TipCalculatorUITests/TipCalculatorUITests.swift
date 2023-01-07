16338700b8538587db5c2cb212dd9d2b9a82acbc
applebuddy <eagermin92@gmail.com>
2023-01-08 01:55:08 +0900

    Study Lecture 4-22. Setting Up User Interface Test Project in Xcode
    learning how to create UI Tests bundle target in Xcode

---
+++ TipCalculator/TipCalculatorUITests/TipCalculatorUITests.swift
@@ -0,0 +1,44 @@
+//
+//  TipCalculatorUITests.swift
+//  TipCalculatorUITests
+//
+//  Created by MinKyeongTae on 2023/01/08.
+//
+
+import XCTest
+
+// MARK: - 22. Setting Up User Interface Test Project in Xcode
+// 하나하나가 빠르게 동작하는 Unit Test와는 다르게, UI Test는 Testing에 비교적 많은 시간이 소요될 수 있다.
+
+final class TipCalculatorUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
